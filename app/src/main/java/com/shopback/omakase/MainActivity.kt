a4222302067b968efca717c10749e2f122b4e96e
chabroncano <chabroncano@gmail.com>
2019-11-28 10:22:58 +0800

    initial commit for project omakase

---
+++ app/src/main/java/com/shopback/omakase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shopback.omakase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
